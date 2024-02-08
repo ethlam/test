d888b6bc08a9d17706d610969cacb12a822edb0b
ethlam <malthegerstroem@gmail.com>
2024-02-08 15:08:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import seaborn as sns
 from matplotlib.colors import ListedColormap
 from sklearn import datasets, neighbors
 from sklearn.inspection import DecisionBoundaryDisplay
-
+print("test")
 if __name__ == "__main__":
     n_neighbors = 15
 
